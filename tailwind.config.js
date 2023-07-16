13ad802dca4d529ad198c042fb81ca3e7ac0da99
PedroPaivaDev <pedropaivadev@gmail.com>
2023-07-16 19:29:53 -0300

    feat: start of home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,20 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      boxShadow: {
+        'bgShadow': 'inset 0 0 100px 50px black'
+      },
+      colors: {
+        'moodboard': {
+          'light': '#e7e4df',
+          'dark': '#191613',
+          'orange': '#a05e3b',
+          'salmon': '#e0857c',
+          'musg': '#988359',
+          'gree': '#8a9e89',
+          'yellow': '#9c7a30'
+        }
+      }
     },
   },
   plugins: [],
