0cbc9868992e70b977315943b073f75343919354
PedroPaivaDev <pedropaivadev@gmail.com>
2023-07-29 19:49:18 -0300

    fix: Safari couldn't make the 'Header' component visible when the page was at the top, so now the 'HeaderViewContext' context has a conditional to do this check first;

--- src/contexts/HeaderViewContext.tsx
+++ src/contexts/HeaderViewContext.tsx
@@ -14,10 +14,13 @@ const HeaderViewProvider = ({children}:{children:React.ReactNode}) => {
   
   function handleScroll(event:React.UIEvent<HTMLDivElement>) {
     const currentScrollPos = event.currentTarget.scrollTop;
-    const isVisible = prevScrollPos > currentScrollPos;
-
-    setPrevScrollPos(currentScrollPos);
-    setVisible(isVisible);
+    if(currentScrollPos===0) {
+      setVisible(true);
+    } else {
+      const isVisible = prevScrollPos > currentScrollPos;
+      setPrevScrollPos(currentScrollPos);
+      setVisible(isVisible);
+    }
   };
 
   return (
